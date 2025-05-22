2eca121d6c948615e9db3966a1112ffbbfa18a33
Kamil Gieras <kamil.gieras@onet.pl>
2025-05-22 03:52:00 +0200

    update

---
+++ lua/custom/plugins/debuger.lua
@@ -0,0 +1,112 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap-python",
+		},
+		config = function()
+			local dap = require("dap")
+			local ui = require("dapui")
+
+			dap.adapters.codelldb = {
+				type = "server",
+				port = "${port}",
+				executable = {
+					-- update the path below depending on your OS and setup
+					command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+					args = { "--port", "${port}" },
+				},
+			}
+			require("dapui").setup()
+			require("dap-go").setup()
+
+			require("nvim-dap-virtual-text").setup({
+				-- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+				display_callback = function(variable)
+					local name = string.lower(variable.name)
+					local value = string.lower(variable.value)
+					if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
+						return "*****"
+					end
+
+					if #variable.value > 15 then
+						return " " .. string.sub(variable.value, 1, 15) .. "... "
+					end
+
+					return " " .. variable.value
+				end,
+			})
+
+			dap.configurations.cpp = {
+				{
+					name = "Launch C++ executable",
+					type = "codelldb", -- important!
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopOnEntry = false,
+					args = {},
+
+					-- optional pretty printing
+					setupCommands = {
+						{
+							text = "-enable-pretty-printing",
+							description = "enable pretty printing",
+							ignoreFailures = false,
+						},
+					},
+				},
+			}
+			local python_path = "/opt/homebrew/bin/python3.10"
+			require("dap-python").setup(python_path)
+
+			-- Skróty klawiszowe dla testowania metod i klas w Pythonie
+			vim.keymap.set("n", "<leader>dt", function()
+				require("dap-python").test_method()
+			end, { desc = "Test Python Method" })
+
+			vim.keymap.set("n", "<leader>dc", function()
+				require("dap-python").test_class()
+			end, { desc = "Test Python Class" })
+
+			vim.keymap.set("n", "<leader>ds", function()
+				require("dap").continue()
+			end, { desc = "Continue Debugging" })
+
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>?", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F1>", dap.continue)
+			vim.keymap.set("n", "<F2>", dap.step_into)
+			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F4>", dap.step_out)
+			vim.keymap.set("n", "<F5>", dap.step_back)
+			vim.keymap.set("n", "<F13>", dap.restart)
+
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
+}
